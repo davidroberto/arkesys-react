c5e15fd5ab92560fd7734411c4933cd0fd7a38ff
David Robert <contact@david-robert.fr>
2026-06-08 12:01:37 +0200

    add route for homePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header.tsx";
+
+const HomePage = () => (
+    <>
+        <Header />
+
+        <section>
+            <h4>Ce site présente tous les events du 4L trophy</h4>
+            <button>Accéder à la page des events</button>
+        </section>
+    </>
+);
+
+export default HomePage;
